bf46d33484690333600aba7eb6893fadd65b3683
Iryna Slynko <iryna.slynko@gmail.com>
2021-06-14 00:05:04 +0100

    Fix import

--- server/__init__.py
+++ server/__init__.py
@@ -1,21 +1,16 @@
 from flask import Flask
+from . import db, auth, user, data, client
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret key here'
 
-    from . import db
     db.init_app(app)
-
-    from . import auth
     app.register_blueprint(auth.bp)
-
-    from . import user
     app.register_blueprint(user.bp)
-
-    from . import data
     app.register_blueprint(data.bp)
+    app.register_blueprint(client.bp)
 
     @app.route('/')
     def index():
