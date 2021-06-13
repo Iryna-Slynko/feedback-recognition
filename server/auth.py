4a911a4aa89290d207d9719715f41a61c21a93ea
Iryna Slynko <iryna.slynko@gmail.com>
2021-06-14 00:07:08 +0100

    Remove unused methods

--- server/auth.py
+++ server/auth.py
@@ -3,7 +3,7 @@ import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash
 
 from server.db import get_db
 
