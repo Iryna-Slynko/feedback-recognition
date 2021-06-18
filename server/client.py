aa65aa7ad85995f38a065b8d11263087e077de90
Iryna Slynko <iryna.slynko@gmail.com>
2021-06-18 23:10:01 +0100

    Fix client create
    * Add proper fields
    * Fix parameters for creation
    * Validate missing parameters before checking uniqueness

--- server/client.py
+++ server/client.py
@@ -13,24 +13,28 @@ bp = Blueprint('client', __name__, url_prefix='/client')
 @admin_required
 def create():
     if request.method == 'POST':
-        client_name = request.form['client']
-        location_id = request.form['location_id']
+        client_name = request.form.get('client_name')
+        location_id = request.form.get('location_id')
         db = get_db()
         error = None
-        if (
-            db.execute("SELECT id FROM client WHERE client = ?",
+        if not client_name:
+            error = 'Please enter the client name'
+        elif not location_id:
+            error = 'Please enter the location id'
+        elif (
+            db.execute("SELECT client_id FROM client WHERE client = ?",
                        (client_name,)).fetchone()
             is not None
         ):
-            error = 'User already exists.'
+            error = 'Client already exists.'
         if error is None:
             token = token_urlsafe(30)
             db.execute(
-                "INSERT INTO client (client, token, token_start, location_id) VALUES (?, ?, ?)",
+                "INSERT INTO client (client, token, token_start, location_id) VALUES (?, ?, ?, ?)",
                 (client_name, generate_password_hash(token), token[0:3], location_id),
             )
             db.commit()
-            return redirect(url_for('user.index'))
+            return render_template('client/show.html', token=token, client_name=client_name)
 
         flash(error)
 
