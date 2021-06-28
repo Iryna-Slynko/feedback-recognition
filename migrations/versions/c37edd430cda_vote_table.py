eb0da413bc182731d8a6adb99808fcf8945036f5
Iryna Slynko <iryna.slynko@gmail.com>
2021-06-28 23:43:36 +0100

    Add migrations

---
+++ migrations/versions/c37edd430cda_vote_table.py
@@ -0,0 +1,32 @@
+"""vote_table
+
+Revision ID: c37edd430cda
+Revises: c88e0371ce34
+Create Date: 2021-06-28 22:58:45.991920
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c37edd430cda'
+down_revision = 'c88e0371ce34'
+branch_labels = None
+depends_on = None
+
+
+def downgrade():
+    op.drop_table('vote')
+    
+
+def upgrade():
+     op.create_table('vote',
+    sa.Column('vote_id', sa.INTEGER(), nullable=True),
+    sa.Column('client_id', sa.INTEGER(), nullable=False),
+    sa.Column('created', sa.TIMESTAMP(), server_default=sa.text('(CURRENT_TIMESTAMP)'), nullable=False),
+    sa.Column('upvote', sa.BOOLEAN(), nullable=False),
+    sa.ForeignKeyConstraint(['client_id'], ['client.client_id'], ),
+    sa.PrimaryKeyConstraint('vote_id')
+    )
+    
---
+++ migrations/versions/c88e0371ce34_user_table.py
@@ -0,0 +1,32 @@
+"""user_table
+
+Revision ID: c88e0371ce34
+Revises: c1f12c0ad3ca
+Create Date: 2021-06-28 22:58:39.816612
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c88e0371ce34'
+down_revision = 'c1f12c0ad3ca'
+branch_labels = None
+depends_on = None
+
+def downgrade():
+    op.drop_table('user')
+
+def upgrade():
+        op.create_table('user',
+    sa.Column('user_id', sa.INTEGER(), nullable=True),
+    sa.Column('username', sa.TEXT(), nullable=False),
+    sa.Column('password', sa.TEXT(), nullable=False),
+    sa.Column('role', sa.TEXT(), server_default=sa.text("'user'"), nullable=False),
+    sa.PrimaryKeyConstraint('user_id'),
+    sa.UniqueConstraint('password'),
+    sa.UniqueConstraint('role'),
+    sa.UniqueConstraint('username')
+    )
+
