dbd2164f1fabde439f0b3d3c05e895aedce66446
Iryna Slynko <iryna.slynko@gmail.com>
2021-08-13 23:49:27 +0100

    remove unused class and fix the name

--- recognition/contour_extractor.py
+++ recognition/contour_extractor.py
@@ -1,14 +1,4 @@
 import cv2 as cv
-import json
-from json import JSONEncoder
-import numpy
-
-
-class NumpyArrayEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, numpy.ndarray):
-            return obj.tolist()
-        return JSONEncoder.default(self, obj)
 
 
 def extract_contours(diff):
@@ -19,8 +9,8 @@ def extract_contours(diff):
         canny_output, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE))
 
 
-def filter_contours(tuple):
-    contours, hierarchy = tuple
+def filter_contours(pair):
+    contours, hierarchy = pair
     filtered = []
     if hierarchy is not None and len(hierarchy[0]) > 5:
         subarray = hierarchy[0]
