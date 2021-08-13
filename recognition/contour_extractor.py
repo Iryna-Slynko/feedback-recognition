3624868d5ea2fa00f0d81298b30a5fb905eeea78
Iryna Slynko <iryna.slynko@gmail.com>
2021-08-13 22:33:08 +0100

    Add some filtering to controus
    Make sure it does some filtering but leaves some data

--- recognition/contour_extractor.py
+++ recognition/contour_extractor.py
@@ -1,4 +1,14 @@
 import cv2 as cv
+import json
+from json import JSONEncoder
+import numpy
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
 
 
 def extract_contours(diff):
@@ -13,9 +23,8 @@ def filter_contours(tuple):
     contours, hierarchy = tuple
     filtered = []
     if hierarchy is not None and len(hierarchy[0]) > 5:
-
         subarray = hierarchy[0]
         for i in range(len(subarray)):
-            if subarray[i][3] < 0:
+            if subarray[i][3] < 0 and len(contours[i]) > 10:
                 filtered.append(contours[i])
     return filtered
