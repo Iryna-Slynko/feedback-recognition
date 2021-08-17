0ff38a9db46c5a850cb422bef30adb5c26f21341
Iryna Slynko <iryna.slynko@gmail.com>
2021-08-17 23:58:36 +0100

    Keep history of decisions

--- recognition/repeated_decider.py
+++ recognition/repeated_decider.py
@@ -5,10 +5,12 @@ class RepeatedDecider:
     def __init__(self):
         self.__decider_class__ = Decider
         self.__analyzed_count__ = 0
+        self.__decisions__ = []
 
     def analyze(self, np_array):
         self.__analyzed_count__ += 1
-        pass
+        single_decider = self.__decider_class__(np_array)
+        self.__decisions__.append(single_decider.is_upvote())
 
     def is_decided(self):
         if self.__analyzed_count__ >= 4:
@@ -16,4 +18,11 @@ class RepeatedDecider:
         return False
 
     def is_upvote(self):
+        votes = 0
+        for decision in self.__decisions__:
+            if decision:
+                votes += 1
+        return votes > 0 and (len(self.__decisions__) / votes) <= 2
+
+    def decided_result(self):
         return True
--- test/repeated_decider_test.py
+++ test/repeated_decider_test.py
@@ -6,20 +6,63 @@ class MockUpvoteDecider:
     def __init__(self, np_array):
         pass
 
-    def is_decided():
+    def is_decided(self):
         return True
 
-    def is_upvote():
+    def is_upvote(self):
         return True
+
+
+class MockDownvoteDecider:
+    def __init__(self, np_array):
+        pass
+
+    def is_decided(self):
+        return True
+
+    def is_upvote(self):
+        return False
+
+
+class MockUndecidedDecider:
+    def __init__(self, np_array):
+        pass
+
+    def is_decided(self):
+        return False
+
+    def is_upvote(self):
+        return False
 
 
 class TestRepeatedDecider(unittest.TestCase):
     def test_undecided_in_beginning(self):
         self.assertFalse(RepeatedDecider().is_decided())
 
-    def test_get_decided_if_five_times_gets_upvote(self):
+    def test_get_decided_if_five_times_gets_decided(self):
         decider = RepeatedDecider()
         decider.__decider_class__ = MockUpvoteDecider
         for _ in range(5):
             decider.analyze([])
         self.assertTrue(decider.is_decided())
+
+    def test_show_the_average_result_of_decider_downvote(self):
+        decider = RepeatedDecider()
+        decider.__decider_class__ = MockDownvoteDecider
+        decider.analyze([])
+        self.assertFalse(decider.is_upvote())
+
+    def test_show_the_average_result_of_decider_upvote(self):
+        decider = RepeatedDecider()
+        decider.__decider_class__ = MockUpvoteDecider
+        decider.analyze([])
+        self.assertTrue(decider.is_upvote())
+
+    def test_show_the_average_result_of_decider_mixed(self):
+        decider = RepeatedDecider()
+        decider.__decider_class__ = MockUpvoteDecider
+        decider.analyze([])
+        decider.analyze([])
+        decider.__decider_class__ = MockDownvoteDecider
+        decider.analyze([])
+        self.assertTrue(decider.is_upvote())
