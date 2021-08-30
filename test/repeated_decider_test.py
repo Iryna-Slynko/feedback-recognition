a9cef8f68cde7504c88fa7e97b57267099c7ebe7
Iryna Slynko <iryna.slynko@gmail.com>
2021-08-30 22:31:26 +0100

    Increased number of tries

--- test/repeated_decider_test.py
+++ test/repeated_decider_test.py
@@ -1,6 +1,8 @@
 from recognition.repeated_decider import RepeatedDecider
 import unittest
 
+decision_times = 20
+
 
 class MockUpvoteDecider:
     def __init__(self, np_array):
@@ -39,10 +41,10 @@ class TestRepeatedDecider(unittest.TestCase):
     def test_undecided_in_beginning(self):
         self.assertFalse(RepeatedDecider().is_decided())
 
-    def test_get_decided_if_five_times_gets_decided(self):
+    def test_get_decided_if_multiple_times_gets_decided(self):
         decider = RepeatedDecider()
         decider.__decider_class__ = MockUpvoteDecider
-        for _ in range(5):
+        for _ in range(decision_times):
             decider.analyze([])
         self.assertTrue(decider.is_decided())
 
@@ -70,32 +72,23 @@ class TestRepeatedDecider(unittest.TestCase):
     def test_undecided_if_result_of_seven_latest_is_mixed(self):
         decider = RepeatedDecider()
         decider.__decider_class__ = MockUpvoteDecider
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
+        for _ in range(decision_times // 3 + 1):
+            decider.analyze([])
         decider.__decider_class__ = MockDownvoteDecider
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
+        for _ in range(2 * decision_times // 3 - 1):
+            decider.analyze([])
         self.assertFalse(decider.is_decided())
 
-    def test_decided_if_result_of_more_than_seven_latest_is_mixed_but_last_seven_are_stable(
+    def test_decided_if_result_of_more_than_some_amount_is_mixed_but_than_stable(
         self,
     ):
         decider = RepeatedDecider()
         decider.__decider_class__ = MockUpvoteDecider
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
+        for _ in range(decision_times // 3):
+            decider.analyze([])
         decider.__decider_class__ = MockDownvoteDecider
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
+        for _ in range(2 * decision_times // 3 + 1):
+            decider.analyze([])
         self.assertTrue(decider.is_decided())
 
     def test_decided_if_result_of_recent_latest_undecided(
@@ -103,26 +96,21 @@ class TestRepeatedDecider(unittest.TestCase):
     ):
         decider = RepeatedDecider()
         decider.__decider_class__ = MockUndecidedDecider
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
-        decider.analyze([])
+        for _ in range(decision_times):
+            decider.analyze([])
         self.assertFalse(decider.is_decided())
 
     def test_refresh_resets_decision_and_skips_hundred_next_ones(self):
         decider = RepeatedDecider()
         decider.__decider_class__ = MockUpvoteDecider
-        for _ in range(5):
+        for _ in range(decision_times):
             decider.analyze([])
         self.assertTrue(decider.is_decided())
         decider.reset()
         for _ in range(100):
             decider.analyze([])
             self.assertFalse(decider.is_decided())
-        for _ in range(5):
+        for _ in range(decision_times):
             decider.analyze([])
         self.assertTrue(decider.is_decided())
 
